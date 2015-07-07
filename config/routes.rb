bd5e1b9b2f1ee1293defa920157acdc1293c777f
Victor Vitalino <vitalino.victor@gmail.com>
2015-07-07 17:30:34 -0300

    scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,17 @@
 Rails.application.routes.draw do
 
+   namespace :cms, path: 'conteudo' do
+     root 'pages#index'
+     resources :posts, path: 'postagens'
+     resources :post_categories, path: 'postagem_categorias'
+
+     resources :pages, path: 'paginas'
+     resources :page_categories, path: 'pagina_categorias'
+
+  #   resources :users, path: 'usuario'
+   end
+
     namespace :content, path: '/' do
-    root 'home#index'
+      root 'home#index'
     end
 end
